7f449c86c8fa3cd8fc67ccb7a1d04ea05fd519d0
imed-tarhouni <tarhouni.imed@gmail.com>
2017-08-01 22:25:00 +0200

    Update angular-snap.js

--- angular-snap.js
+++ angular-snap.js
@@ -160,6 +160,22 @@ angular.module('snap')
 
 
 angular.module('snap')
+  .directive('snapRoot', ['snapRemote', function(snapRemote) {
+    'use strict';
+    return {
+      restrict: 'AE',
+      link: function(scope, element, attrs) {
+        var snapId = scope.$eval(attrs.snapId);
+        snapRemote.getSnapper(snapId).then(function(snapper) {
+          snapper.settings({
+            rootElement: element[0]
+          });
+        });
+      }
+    };
+  }]);
+
+angular.module('snap')
   .directive('snapToggle', ['$rootScope', 'snapRemote', function($rootScope, snapRemote) {
       'use strict';
       return {
